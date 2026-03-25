86240f0686e6daf2d03cfef9e104df61b15bc59e
Ana Beatriz <beatriiz008@hotmail.com>
2026-03-24 22:05:15 -0300

    pendente pratica2

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,43 @@
 import './style.css'
+
+const form = document.getElementById("loginForm");
+const matricula = document.getElementById("matricula");
+const password = document.getElementById("senha");
+
+
+function showError(input, message) {
+  const fieldset = input.parentElement;
+  const errorSpan = fieldset.querySelector(".error-message");
+
+  errorSpan.textContent = message;
+  input.classList.add("error");
+}
+
+function clearError(input) {
+  const fieldset = input.parentElement;
+  const errorSpan = fieldset.querySelector(".error-message");
+
+  errorSpan.textContent = "";
+  input.classList.remove("error");
+}
+
+function validate(input) {
+  if (input.value.trim() === "") {
+    showError(input, "Campo obrigatório");
+    return false;
+  }
+
+  clearError(input);
+  return true;
+}
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); // impede recarregar página
+
+  const matriculaValid = validate(matricula);
+  const passwordValid = validate(password);
+
+  if (matriculaValid && passwordValid) {
+    window.location.href = "dashboard.html";
+  }
+});
