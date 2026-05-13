a0324442478eb8e95d3e4898b5724caea5a6546c
Ana Beatriz <beatriiz008@hotmail.com>
2026-05-12 21:16:04 -0300

    adiciona contexto de auth

--- aulas/aula04/src/components/Menu.jsx
+++ aulas/aula04/src/components/Menu.jsx
@@ -1,11 +1,21 @@
+import { useContext } from "react";
 import "./Menu.css";
 import { Link, NavLink } from "react-router";
+import { AuthContext } from "../contexts/AuthContext";
 
 function Menu() {
   const usuarioId = 0; //pegou da API
 
+  const {logout, usuario} = useContext(AuthContext);
+
+  const handleSair = () => {
+    logout();
+  }
+
   return (
     <nav>
+      <h4>{usuario.nome}</h4>
+      <h5>{usuario.email}</h5>
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
@@ -20,7 +30,7 @@ function Menu() {
           <NavLink to="/settings">Configurações</NavLink>
         </li>
         <li>
-          <Link to="/login">Sair</Link>
+          <Link to="/" onClick={handleSair}>Sair</Link>
         </li>
       </ul>
     </nav>
