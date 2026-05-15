7a720eb82cccb8531245db0a8ba7fad30cb732b6
Ana Beatriz <beatriiz008@hotmail.com>
2026-05-14 21:03:23 -0300

    corrigir projeto aluno react

--- praticas/aluno-online-react/src/App.jsx
+++ praticas/aluno-online-react/src/App.jsx
@@ -1,7 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
-
+import { Routes, Route } from 'react-router'
 import Layout from './components/Layout/Layout'
-
 import Dashboard from './pages/Dashboard/Dashboard'
 import Faltas from './pages/Faltas/Faltas'
 import Notas from './pages/Notas/Notas'
